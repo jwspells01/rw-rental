e38143553dbced638d428ea3218ced258a6904bb
jwspells01 <118319982+jwspells01@users.noreply.github.com>
2023-04-11 23:50:11 -0500

    Add files via upload
    Modified language to English. Changed NPC character. To add vehicles, add your vehicles to the config.lua, screenshot a .jpg and add to html images.

---
+++ rw-rental/config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.CarSpawn = {
+    vector4(-1232.49, -347.91, 37.33, 21.91),
+}
+
+Config.PedSpawn = {
+    vector4(-1257.52, -369.12, 36.91, 31.12),
+}
+
+Config.Vehicles = {
+    {name = "Toyota Prius", image = "prius.jpg", price = 50, hash = "prius"},
+    {name = "Toyota Camry", image = "camry18.jpg", price = 100, hash = "camry18"},
+    {name = "GMC Yukon", image = "gmcyd.jpg", price = 150, hash = "gmcyd"},
+    {name = "Chevy Camaro", image = "zl12017.jpg", price = 200, hash = "zl12017"},
+}
+
